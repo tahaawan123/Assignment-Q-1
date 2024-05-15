480ea307636aeca7666d022baeb80ffaa54d4617
MUHAMMAD TAHA <160233035+tahaawan123@users.noreply.github.com>
2024-05-16 02:14:49 +0500

    Add files via upload

---
+++ Q1/main.ts
@@ -0,0 +1,14 @@
+// Q1. Miles-to-Kilometers Converter (Operators):
+
+// • Create a variable named miles and assign a numeric value representing distance in miles.
+// • Calculate the equivalent distance in kilometers using the conversion factor (1 mile = 
+// 1.60934 kilometers). You can achieve this by multiplying miles by 1.60934.
+// • Store the converted distance in kilometers in a variable named kilometers.
+// • Use console.log to print a message in the following format: 
+// The distance of 130 kms is equal to 209.2142 miles
+
+
+
+let miles:number=130;
+let kiloMeters= miles * 1.60934;
+console.log(`The distance of ${miles} miles is equal to ${kiloMeters} kms `);
